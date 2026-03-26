1d9adc6d9cee9da48aae13daa52c541450746ff2
kumaarjoo <kumaarjoo@gmail.com>
2026-03-26 21:36:38 +1300

    app.py

--- app.py
+++ app.py
@@ -1,8 +1,1 @@
-import streamlit as st
-import pandas as pd
-import plotly.express as px
 
-st.title("Global Climate Change Dashboard")
-df = pd.read_csv("GlobalLandTemperaturesByCountry.csv")
-# ... analysis and interactive widgets
-st.plotly_chart(fig)
